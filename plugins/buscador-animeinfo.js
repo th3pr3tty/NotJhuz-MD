53afb288a72ae9ab6bd8b046f3ce4c78f4b1cc2d
th3pr3tty <107769370+th3pr3tty@users.noreply.github.com>
2022-08-14 20:14:31 -0500

    Update buscador-animeinfo.js

--- plugins/buscador-animeinfo.js
+++ plugins/buscador-animeinfo.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 import cheerio from 'cheerio'
 let handler = async (m, { conn, text }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴𝙻 𝙰𝙻𝙶𝚄𝙽 𝙰𝙽𝙸𝙼𝙴 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*`
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] Ingrese el nombre del anime que desea buscar.*`
 let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/anime', { q: text }))
 if (!res.ok) throw await res.text()
 let json = await res.json()
@@ -9,14 +9,14 @@ let { title, members, synopsis, episodes, url, rated, score, image_url, type, st
 let res2 = await fetch(`https://myanimelist.net/anime/${mal_id}`)
 if (!res2.ok) throw await res2.text()
 let html = await res2.text()
-let animeingfo = `✨ *Titulo:* ${title}
-🎆 *Episodios:* ${episodes}
-💬 *Transmitido en:* ${type}
-💌 *Rating:* ${rated}
-❤️ *Score:* ${score}
-👥 *Miembros:* ${members}
-💚 *Sinopsis en inglés:* ${synopsis}
-🌐 *URL*: ${url}`
+let animeingfo = `✨ Título: ${title}
+🎆 Episodios: ${episodes}
+💬 Transmitido en: ${type}
+💌 Rating: ${rated}
+❤️ Score: ${score}
+👥 Miembros: ${members}
+💚 Sinopsis en inglés: ${synopsis}
+🌐 URL: ${url}`
 conn.sendFile(m.chat, image_url, '', animeingfo, m)
 }
 handler.help = ['animeinfo <anime>']
